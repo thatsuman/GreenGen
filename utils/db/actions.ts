1535df53d0d007035ec6befd228a5c35445f99b7
Suman Mondal <suman.mandal1020@gmail.com>
2025-03-30 15:00:17 +0530

    add: actions

---
+++ utils/db/actions.ts
@@ -0,0 +1,9 @@
+import { db } from "./dbConfig";
+import { Users } from "./schema";
+import { eq, sql, and, desc } from "drizzle-orm";
+
+export async function createUser(email: string, name: string){
+    try {
+        //start here @rubu18
+    }
+}
