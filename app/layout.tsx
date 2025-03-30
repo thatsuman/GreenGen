edc15de75e50c9565ebbcf715bce84505ebed703
Suman Mondal <suman.mandal1020@gmail.com>
2025-03-30 17:06:53 +0530

    add: sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,10 @@
 
 import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
-
 import "./globals.css";
 
-//header
-//sidebar
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 import { Toaster } from "react-hot-toast";
 
@@ -20,13 +19,42 @@ export default function RootLayout({
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [totalEarnings, setTotalEarnings] = useState(0);
 
+  useEffect(() => {
+    const fetchTotalEarnings = async () => {
+      try {
+        const userEmail = localStorage.getItem("userEmail");
+        if (userEmail) {
+          const user = await getUserByEmail(userEmail);
+          console.log("user from layout", user);
+
+          if (user) {
+            const availableRewards = (await getAvailableRewards(
+              user.id
+            )) as any;
+            console.log("availableRewards from layout", availableRewards);
+            setTotalEarnings(availableRewards);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching total earnings:", error);
+      }
+    };
+
+    fetchTotalEarnings();
+  }, []);
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <div className="min-h-screen bg-grey-50 flex flex-col">
           {/* header */}
+          <Header
+            onMenuClick={() => setSidebarOpen(!sidebarOpen)}
+            totalEarnings={totalEarnings}
+          />
           <div className="flex flex-1">
             {/* sidebar */}
+            <Sidebar open={sidebarOpen} />
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
               {children}
             </main>
